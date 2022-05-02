f27680a8e8692b333b082fdfaf871916ea934e63
brikbrown <brianna.kbrown@yahoo.com>
2022-05-01 21:15:52 -0400

    replace mat with primeng components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,34 @@
+// Angular Imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import {
-  HttpClientModule,
-  HttpErrorResponse,
-  HTTP_INTERCEPTORS,
-} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+//Module Imports
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { GaugeModule } from 'angular-gauge';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+
+// Component Imports
+import { AppComponent } from './app.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
-import { RouterModule } from '@angular/router';
 import { HttpHeadersInterceptors } from './interceptors/http-headers.interceptors';
 import { HttpErrorsInterceptors } from './interceptors/http-errors.interceptors';
+import { DetailsComponent } from './components/details/details.component';
+
 
 @NgModule({
-  declarations: [AppComponent, SearchBarComponent, HomeComponent],
+  declarations: [AppComponent, SearchBarComponent, HomeComponent, DetailsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -36,6 +41,10 @@ import { HttpErrorsInterceptors } from './interceptors/http-errors.interceptors'
     MatFormFieldModule,
     MatSelectModule,
     RouterModule,
+    DropdownModule,
+    InputTextModule,
+    ButtonModule,
+    TooltipModule
   ],
   providers: [
     {
